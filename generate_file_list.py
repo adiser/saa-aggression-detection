db85b71421f72b002fc4bf96bc26e2dd64eb221c
adiser <sergiadipraja@gmail.com>
2018-12-07 16:40:23 +0800

    Added video demo

--- generate_file_list.py
+++ generate_file_list.py
@@ -4,8 +4,8 @@ import os
 from sklearn.model_selection import StratifiedKFold
 import numpy as np
 
-pos = glob.glob('frames/aggressive_rgbd/*')
-neg = glob.glob('frames/passive_rgbd/*')
+pos = glob.glob('frames/aggressive/*')
+neg = glob.glob('frames/passive_cctv/*')
 
 x = pos + neg
 y = [1] * len(pos) + [0] * len(neg)
@@ -20,14 +20,14 @@ for i, (train_idx, test_idx) in enumerate(kf.split(X, y)):
     X_train = X[train_idx]
     y_train = y[train_idx]
     
-    with open('data/rgbd_train_split_{}.txt'.format(i), 'w') as fh:
+    with open('data/cctv_train_split_{}.txt'.format(i), 'w') as fh:
         for j in range(len(X_train)):
             fh.write('{} {}\n'.format(X_train[j], y_train[j]))
             
     X_test = X[test_idx]
     y_test = y[test_idx]
     
-    with open('data/rgbd_test_split_{}.txt'.format(i), 'w') as fh:
+    with open('data/cctv_test_split_{}.txt'.format(i), 'w') as fh:
         for j in range(len(X_test)):
             fh.write('{} {}\n'.format(X_test[j], y_test[j]))
 
